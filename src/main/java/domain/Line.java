98cbe227618baca7d7e18484e2f8f70a49fd7d39
김경록 <goodboy3421@gmail.com>
2020-08-27 08:10:22 +0900

    refactor: Line 객체 생성 로직 분리
    Line 객체를 생성하는 strategy 추가한다. (points를 생성하여 Line 객체 생성자로 바로 주입한다)

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -2,45 +2,20 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Line {
-    public static final String LESS_THEN_MIN_PERSON_NUMBER_MESSAGE = "최소 2명의 참가자가 필요합니다.";
-    public static final int MIN_PERSON_NUMBER = 2;
-    public static final int ZERO = 0;
+    private static final String LESS_THEN_MIN_PERSON_NUMBER_MESSAGE = "최소 2명의 참가자가 필요합니다.";
+    private static final int MIN_POINT_NUMBER = 1;
 
-    private final List<Boolean> points = new ArrayList<>();
-    private final Random random = new Random();
+    private final List<Boolean> points;
 
-    public Line(int countOfPerson) {
-        validatePersonNumber(countOfPerson);
-        generatePoints(countOfPerson);
+    public Line(List<Boolean> points) {
+        validatePersonNumber(points);
+        this.points = new ArrayList<>(points);
     }
 
-    private void generatePoints(int countOfPerson) {
-        for (int i = 0; i < countOfPerson - 1; i++) {
-            addPoint(i);
-        }
-    }
-
-    private void addPoint(int currentIndex) {
-        if (isFirstIndex(currentIndex) || isBeforeValueThatFalse(currentIndex)) {
-            points.add(random.nextBoolean());
-            return;
-        }
-        points.add(false);
-    }
-
-    private boolean isBeforeValueThatFalse(int currentIndex) {
-        return !points.get(currentIndex - 1);
-    }
-
-    private boolean isFirstIndex(int currentIndex) {
-        return currentIndex == ZERO;
-    }
-
-    private void validatePersonNumber(int countOfPerson) {
-        if (countOfPerson < MIN_PERSON_NUMBER) {
+    private void validatePersonNumber(List<Boolean> points) {
+        if (points.size() < MIN_POINT_NUMBER) {
             throw new IllegalArgumentException(LESS_THEN_MIN_PERSON_NUMBER_MESSAGE);
         }
     }
--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -3,31 +3,35 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
-    @DisplayName("Line 객체를 선언한다")
+    @DisplayName("Line 객체를 생성한다")
     @Test
     void testInitLine() {
         //given
-        int personNumber = 3;
+        List<Boolean> points = Arrays.asList(true, false, true);
 
         //when
-        Line line = new Line(personNumber);
+        Line line = new Line(points);
 
         //then
-        assertThat(line.getPoints()).hasSize(personNumber - 1);
+        assertThat(line.getPoints()).hasSize(points.size());
     }
 
-    @DisplayName("Line객체는  최소 참가인원보다 적으면 예외를 발생시킨다")
+    @DisplayName("포인트가 하나도 없으면 예외를 발생시킨다")
     @Test
     void testPersonNumberLessThanMinPersonNumber() {
         //given
-        int personNumber = 1;
+        List<Boolean> points = Collections.EMPTY_LIST;
 
         //when then
-        assertThatThrownBy(() -> new Line(personNumber))
+        assertThatThrownBy(() -> new Line(points))
                 .isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
