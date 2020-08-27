8bbc2a280f1636c71a6b50a0380fef7cf265e2c8
김경록 <goodboy3421@gmail.com>
2020-08-27 18:50:36 +0900

    feat: Points 일급 컬렉션 추가
    Line의 예외처리(연속으로 연결된 Point가 있는 경우)를 Points의 예외로 처리한다

---
+++ src/main/java/domain/Points.java
@@ -0,0 +1,47 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Points {
+    public static final String POINT_NOT_CONTINUITY_CONNECTED_MESSAGE = "Point는 연속해서 연결될 수 없습니다.";
+    private static final String LESS_THEN_MIN_PERSON_NUMBER_MESSAGE = "최소 2명의 참가자가 필요합니다.";
+    private static final int MIN_POINT_NUMBER = 1;
+
+    private final List<Point> points;
+
+    public Points(List<Point> points) {
+        validatePoints(points);
+        this.points = new ArrayList<>(points);
+    }
+
+    private void validatePoints(List<Point> points) {
+        validatePointsSize(points);
+        validatePointsConnectionStatus(points);
+    }
+
+    private void validatePointsSize(List<Point> points) {
+        if (points.size() < MIN_POINT_NUMBER) {
+            throw new IllegalArgumentException(LESS_THEN_MIN_PERSON_NUMBER_MESSAGE);
+        }
+    }
+
+    private void validatePointsConnectionStatus(List<Point> points) {
+        Point beforePoints = new Point(false);
+        for (Point currentPoint : points) {
+            validateFrontAndRearPoints(beforePoints, currentPoint);
+
+            beforePoints = currentPoint;
+        }
+    }
+
+    private void validateFrontAndRearPoints(Point beforePoints, Point currentPoint) {
+        if (beforePoints.isConnected() && currentPoint.isConnected()) {
+            throw new IllegalArgumentException(POINT_NOT_CONTINUITY_CONNECTED_MESSAGE);
+        }
+    }
+
+    public int getSize() {
+        return points.size();
+    }
+}
---
+++ src/test/java/domain/PointsTest.java
@@ -0,0 +1,49 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PointsTest {
+
+    @DisplayName("Points 객체를 생성한다 ")
+    @Test
+    void testInitPoints() {
+        //given
+        List<Point> pointGroup = Arrays.asList(new Point(true)); //todo: 변수명이 중복되는데 뭐라고 정할까??
+
+        //when
+        Points points = new Points(pointGroup);
+
+        //then
+        assertThat(points.getSize()).isEqualTo(1);
+    }
+
+    @DisplayName("포인트가 최소 포인트 갯수보다 적으면 예외를 발생한다 ")
+    @Test
+    void testInitPointsWhenPointNumberLessThanMinPointNumber() {
+        //given
+        List<Point> points = Collections.EMPTY_LIST;
+
+        //when //then
+        assertThatThrownBy(() -> new Points(points))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("포인트가 연속으로 연결되어 있으면 예외를 발생한다 ")
+    @Test
+    void testInitPointIsConnectedContinuously() {
+        //given
+        List<Point> continuouslyConnectedPoints = Arrays.asList(new Point(true), new Point(true));
+
+        //when //then
+        assertThatThrownBy(() -> new Points(continuouslyConnectedPoints))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+}
