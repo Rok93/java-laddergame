c93c88f4e83ced64e58fb1999cf5f624f471a0dd
김경록 <goodboy3421@gmail.com>
2020-08-31 11:38:39 +0900

    refactor: Line 객체가 가지고 있던 책임을 Points 일급 컬렉션에게 이전
    책임이전에 따른 리팩토링 수행

--- src/main/java/domain/Line.java
+++ src/main/java/domain/Line.java
@@ -1,26 +1,13 @@
 package domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Line {
-    private static final String LESS_THEN_MIN_PERSON_NUMBER_MESSAGE = "최소 2명의 참가자가 필요합니다.";
-    private static final int MIN_POINT_NUMBER = 1;
+    private final Points points;
 
-    private final List<Boolean> points;
-
-    public Line(List<Boolean> points) {
-        validatePersonNumber(points);
-        this.points = new ArrayList<>(points);
+    public Line(Points points) {
+        this.points = new Points(points.getValues());
     }
 
-    private void validatePersonNumber(List<Boolean> points) {
-        if (points.size() < MIN_POINT_NUMBER) {
-            throw new IllegalArgumentException(LESS_THEN_MIN_PERSON_NUMBER_MESSAGE);
-        }
-    }
-
-    public List<Boolean> getPoints() {
-        return new ArrayList<>(points);
+    public Points getPoints() {
+        return new Points(points.getValues());
     }
 }
--- src/test/java/domain/LineTest.java
+++ src/test/java/domain/LineTest.java
@@ -4,34 +4,25 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LineTest {
     @DisplayName("Line 객체를 생성한다")
     @Test
     void testInitLine() {
         //given
-        List<Boolean> points = Arrays.asList(true, false, true);
+        List<Point> ts = Arrays.asList(true, false).stream()
+                .map(Point::new)
+                .collect(Collectors.toList());
+        Points points = new Points(ts);
 
         //when
         Line line = new Line(points);
 
         //then
-        assertThat(line.getPoints()).hasSize(points.size());
-    }
-
-    @DisplayName("포인트가 하나도 없으면 예외를 발생시킨다")
-    @Test
-    void testPersonNumberLessThanMinPersonNumber() {
-        //given
-        List<Boolean> points = Collections.EMPTY_LIST;
-
-        //when then
-        assertThatThrownBy(() -> new Line(points))
-                .isExactlyInstanceOf(IllegalArgumentException.class);
+        assertThat(line.getPoints().getValues()).hasSize(ts.size());
     }
 }
