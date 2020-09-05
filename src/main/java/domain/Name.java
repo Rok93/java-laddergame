a0fee8040fc16d280eb76c0acdb87a6a14157f67
김경록 <goodboy3421@gmail.com>
2020-09-06 08:47:11 +0900

    refactor: 예외 출력 메시지 상수명 리팩토링
    의미가 통일성이 없는 것 같아 수정

--- src/main/java/domain/Name.java
+++ src/main/java/domain/Name.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 public class Name {
     public static final int LIMIT_MAX_NAME_LENGTH = 5;
     public static final int LIMIT_MIN_NAME_LENGTH = 1;
-    public static final String OVER_NAME_LENGTH_LIMIT_MESSAGE = "사람의 이름은 최소 1글자 이상 최대 5글까지만 가능합니다.";
+    public static final String NAME_LENGTH_RANGE_LIMIT_MESSAGE = "사람의 이름은 최소 1글자 이상 최대 5글까지만 가능합니다.";
 
     private final String name;
 
@@ -16,7 +16,7 @@ public class Name {
 
     private void validateName(String name) {
         if (name.length() > LIMIT_MAX_NAME_LENGTH || name.length() < LIMIT_MIN_NAME_LENGTH) {
-            throw new IllegalArgumentException(OVER_NAME_LENGTH_LIMIT_MESSAGE);
+            throw new IllegalArgumentException(NAME_LENGTH_RANGE_LIMIT_MESSAGE);
         }
     }
 
--- src/main/java/domain/Result.java
+++ src/main/java/domain/Result.java
@@ -1,6 +1,8 @@
 package domain;
 
 public class Result {
+    private static final String LESS_THAN_MIN_LETTER_NUMBER_MESSAGE = "결괏값은 최소 1자이상 입력해야 합니다. ";
+
     private final String resultValue;
 
     public Result(String resultValue) {
@@ -10,7 +12,7 @@ public class Result {
 
     private void validateResult(String resultValue) {
         if (resultValue.isEmpty()) {
-            throw new IllegalArgumentException("결괏값은 최소 1자이상 입력해야 합니다. ");
+            throw new IllegalArgumentException(LESS_THAN_MIN_LETTER_NUMBER_MESSAGE);
         }
     }
 
