631a06311ee132509deb1bd982347185983a3e37
김경록 <goodboy3421@gmail.com>
2020-09-08 21:20:44 +0900

    feat: 사다리 게임을 실행하는 기능 추가
    실행하면 이름과 결과를 매칭한 결괏값들을 가지는 Map<Name, Result> 결과를 반환한다 + 테스트 코드 (리팩토링 예정)

--- src/main/java/domain/Ladder.java
+++ src/main/java/domain/Ladder.java
@@ -1,15 +1,28 @@
 package domain;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Ladder {
+    private static final String NAMES_NUMBER_IS_NOT_EQUAL_RESULTS_NUMBER_MESSAGE = "사람 수와 결과 갯수는 같아야 합니다.";
+
     private final Names names;
     private final Lines lines;
+    private final Results results;
 
-    public Ladder(Names names, Lines lines) {
-        this.names = new Names(names.getValues());
+    public Ladder(Names names, Lines lines, Results results) {
+        validateLadder(names, results);
+        this.names = names;
         this.lines = lines;
+        this.results = results;
+    }
+
+    private void validateLadder(Names names, Results results) {
+        if (names.calculatePersonNumber() != results.getSize()) {
+            throw new IllegalArgumentException(NAMES_NUMBER_IS_NOT_EQUAL_RESULTS_NUMBER_MESSAGE);
+        }
     }
 
     public List<Line> getLines() {
@@ -18,5 +31,41 @@ public class Ladder {
 
     public Names getNames() {
         return new Names(names.getValues());
+    }
+
+    public Map<Name, Result> play() {
+        Map<Name, Result> results = new HashMap<>();
+        int personNumber = names.calculatePersonNumber();
+        for (int startPoint = 0; startPoint < personNumber; startPoint++) {
+            results.put(names.getValues().get(startPoint), traceLadder(startPoint));
+        }
+
+        return results;
+    }
+
+    public Result traceLadder(int currentPoint) {
+        List<Line> lines = this.lines.getLines();
+        for (Line line : lines) {
+            currentPoint = getNextPoint(line, currentPoint);
+        }
+
+        return results.getValues().get(currentPoint);
+    }
+
+    private int getNextPoint(Line line, int currentPoint) {
+        List<Point> lineValues = line.getPoints().getValues();
+        if (currentPoint <= 0) {
+            return lineValues.get(currentPoint).isConnected() ? currentPoint + 1 : currentPoint;
+        }
+
+        if (currentPoint >= names.calculatePersonNumber() - 2) {
+            return lineValues.get(currentPoint - 1).isConnected() ? currentPoint - 1 : currentPoint;
+        }
+
+        if (lineValues.get(currentPoint).isConnected()) {
+            return lineValues.get(currentPoint).isConnected() ? currentPoint + 1 : currentPoint;
+        }
+
+        return lineValues.get(currentPoint - 1).isConnected() ? currentPoint - 1 : currentPoint;
     }
 }
--- src/test/java/domain/LadderTest.java
+++ src/test/java/domain/LadderTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class LadderTest {
@@ -15,18 +17,53 @@ class LadderTest {
         //given
         Height height = new Height(2);
         Lines lines = new Lines(() -> new Line(Arrays.asList(true)), height);
-        Name chulsoo = new Name("철수");
-        Name yeonghee = new Name("영희");
-        Names names = new Names(Arrays.asList(chulsoo, yeonghee));
+        Names names = new Names(Arrays.asList(new Name("철수"), new Name("영희")));
+        Results results = new Results(Arrays.asList(new Result("꽝"), new Result("당첨")));
 
         //when
-        Ladder ladder = new Ladder(names, lines);
+        Ladder ladder = new Ladder(names, lines, results);
 
         //then
         assertAll(
                 () -> assertThat(ladder.getLines()).hasSize(height.getValue()),
-                () -> assertThat(ladder.getNames().getValues().get(0)).isEqualTo(chulsoo),
-                () -> assertThat(ladder.getNames().getValues().get(1)).isEqualTo(yeonghee)
+                () -> assertThat(ladder.getNames().getValues().contains(new Name("철수"))).isTrue(),
+                () -> assertThat(ladder.getNames().getValues().contains(new Name("영희"))).isTrue()
+                // Results 관련 테스트 추가해야할까??
+        );
+    }
+
+    @DisplayName("Names 길이와 Results 길이가 다르면 예외를 발생한다 ")
+    @Test
+    void testInitWhenNamesNumberIsNotEqualToResultsNumber() {
+        //given
+        Height height = new Height(2);
+        Lines lines = new Lines(() -> new Line(Arrays.asList(true)), height);
+        Names names = new Names(Arrays.asList(new Name("철수"), new Name("영희"), new Name("바둑이")));
+        Results results = new Results(Arrays.asList(new Result("꽝"), new Result("당첨")));
+
+        //when // then
+        assertThatThrownBy(() -> new Ladder(names, lines, results))
+                .isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("사다리 게임을 실행한 결괏값을 얻는 기능 ")
+    @Test
+    void testPlayLadderGame() {
+        //given
+        Height height = new Height(2);
+        Lines lines = new Lines(() -> new Line(Arrays.asList(true)), height);
+        Names names = new Names(Arrays.asList(new Name("철수"), new Name("영희")));
+        Results results = new Results(Arrays.asList(new Result("꽝"), new Result("당첨")));
+
+        Ladder ladder = new Ladder(names, lines, results);
+
+        //when
+        Map<Name, Result> resultsByName = ladder.play();
+
+        //then
+        assertAll(
+                () -> assertThat(resultsByName.get(new Name("철수")).getValue()).isEqualTo("꽝"),
+                () -> assertThat(resultsByName.get(new Name("영희")).getValue()).isEqualTo("당첨")
         );
     }
 }
