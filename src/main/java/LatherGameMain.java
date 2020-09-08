4beca7589c3012c180b086154d7a8ee54a100264
김경록 <goodboy3421@gmail.com>
2020-09-09 05:25:48 +0900

    refactor: 추가된 기능들에 따라 Controller 리팩토링

--- src/main/java/LatherGameMain.java
+++ src/main/java/LatherGameMain.java
@@ -1,10 +1,28 @@
-import domain.Names;
+import domain.*;
 import view.InputView;
+import view.OutputView;
+
+import java.util.Map;
 
 public class LatherGameMain {
     public static void main(String[] args) {
         Names names = InputView.inputPersonNames();
-//        Ladder ladder = new Ladder(InputView.inputHeight(), names.calculatePersonNumber());
-//        OutputView.printResult(ladder, names);
+        Results results = InputView.inputResults();
+        Height height = InputView.inputHeight();
+        Lines lines = new Lines(new DefaultLineGenerator(names), height);
+        Ladder ladder = new Ladder(names, lines, results);
+        OutputView.printLadder(ladder, names, results);
+        Map<Name, Result> ladderResults = ladder.play();
+
+        while (true) {
+            Name name = InputView.inputPersonName();
+            OutputView.printResult(name, ladderResults);
+
+            if (InputView.inputStartOrExit()) {
+                break;
+            }
+        }
+
+        OutputView.printEnd();
     }
 }
